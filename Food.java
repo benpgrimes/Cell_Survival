6869d5b8cdc06f91e1bfe329a543fbec57badb3c
Benjamin Grimes <42780508+EisSchnee@users.noreply.github.com>
2019-02-22 16:08:16 -0500

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,8 +1,6 @@
 
 public class Food extends Cell{
-  private double x, y, diam;
-  private int energy;
-  private color;
+  
   public Food (double x, double y, double diam, int energy){
     this.x = x;
     this.y = y;

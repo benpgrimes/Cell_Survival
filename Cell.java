9d8f78649f3c76d5274875306f7a09d54046a538
Benjamin Grimes <42780508+EisSchnee@users.noreply.github.com>
2019-02-22 16:12:29 -0500

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -1,4 +1,8 @@
-
+/*************************************
+* 
+* Parent of Food and Organism
+*
+***************************/
 public abstract class Cell {
   private double x, y, diam;
   private int energy;

3085b91e5e6fc3feb5156250be8bd824238f49fb
Nicci Vyea Casenas <44342843+niccivyea@users.noreply.github.com>
2019-02-22 17:18:35 -0500

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -10,5 +10,44 @@ public abstract class Cell {
   protected int energy;
   protected Color color;
   
+  public void setX (double x){ //mutator
+    this.x = x;
+  }
+  
+  public void setY (double y){ //mutator
+    this.y = y;
+  }
+  
+  public void setDiam (double diam){ //mutator
+    this.diam = diam;
+  }
+  
+  public void setEnergy (double energy){ //mutator
+    this.energy = energy;
+  }
+  
+  public void setColor (Color color){ //mutator
+    this.color = color;
+  }
+  
+  public double getX(){ //accessor
+    return x;
+  }
+  
+  public double getY(){ //accessor
+    return y;
+  }
+  
+  public double getDiam(){ //accessor
+    return diam;
+  }
+  
+  public int getEnergy(){ //accessor
+    return energy;
+  }
+  
+  public Color getColor(){ //accessor
+    return color;
+  } 
   public abstract void draw(Graphics myBuffer);
 }

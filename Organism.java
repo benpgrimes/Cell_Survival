a2b03f46850c68ef1173c507c0360582ae077062
Benjamin Grimes <42780508+EisSchnee@users.noreply.github.com>
2019-04-08 01:28:58 -0400

    Update Organism.java

--- Organism.java
+++ Organism.java
@@ -21,7 +21,7 @@ public class Organism extends Cell {
 	// x^2 based on last use
 	private int curiosity;
 
-	// minimum for splitting
+	// minimum for ting
 	// x
 	private int maternalMin;
 
@@ -40,9 +40,9 @@ public class Organism extends Cell {
 	// private double closestFoodX;
 	// private double closestFoodY;
 
-	private Food[] closestFood = new Food[3];
+	private Cell[] closestFood = new Food[3];
 
-	private Organism[] closestOrganism = new Organism[3];
+	private Cell[] closestOrganism = new Organism[3];
 
 	// How these values change when the organism is at critical energy levels
 	// [0]; energy level required to activate
@@ -98,11 +98,15 @@ public class Organism extends Cell {
 	 * Description: Puts all the tendencies into a single array
 	 * 
 	 * @param None
-	 * @return integer array of all tendencies (15 values) in the order shown: [0];
-	 *         energy level required to activate [1]; new growth limit [2]; new
-	 *         growth inclination [3]; new curiosity [4]; new maternal minimum [5];
-	 *         new maternal inclination [6]; new active level [7]; new temperament
-	 *         [8-14] preservation in same order
+	 * @return integer array of all tendencies (15 values) in the order shown: 
+	 * [0]; new growth limit 
+	 * [1]; new growth inclination 
+	 * [2]; new curiosity 
+	 * [3]; new maternal minimum 
+	 * [4]; new maternal inclination 
+	 * [5]; new active level 
+	 * [6]; new temperament
+	 * [7-14] preservation in same order
 	 *******************************************/
 	public int[] getTendencies() {
 		int[] tendencies = new int[15];
@@ -123,8 +127,13 @@ public class Organism extends Cell {
 	 * returns the choice made as well as makes its next choice.
 	 * 
 	 * @param None
-	 * @return returns an integer based on the choice. Follows this pattern: 1: Grow
-	 *         2: examine 3: split 4: move 5: idle -1: choice failure
+	 * @return returns an integer based on the choice. Follows this pattern: 
+	 * 1: Grow
+	 * 2: examine 
+	 * 3: split 
+	 * 4: move 
+	 * 5: idle 
+	 * -1: choice failure
 	 ***********************************************/
 	public int choose() {
 		boolean first;
@@ -153,18 +162,21 @@ public class Organism extends Cell {
 		}
 		do {
 			int ctr = 0;
-			int[] values = new int[5];
-			int temp;
-			int total = 0;
+			long[] values = new long[5];
+			long temp;
+			long total = 0;
 			first = this.choice == -1;
 			result = this.choice;
 			int multiplier = 0;
-			if (this.diam >= growthLim)// !< is not a command so i changed it to >=
+			if (this.diam < growthLim)// !< is not a command so i changed it to >=
 			{
 				multiplier = growthInc;
 			}
-			temp = (int) (Math.atan(this.energy) / this.energy);// THIS HAD TO BE MADE AN INT
-			temp = temp * multiplier;
+			//temp = (int) Math.abs((Math.atan(this.energy) / this.energy)*100*multiplier);// THIS HAD TO BE MADE AN INT
+			temp = (int) (growthLim - this.diam) * multiplier;
+			if(temp < 0) {
+				temp = 0;
+			}
 			total += temp;
 			values[ctr] = temp;
 			ctr++;
@@ -176,22 +188,31 @@ public class Organism extends Cell {
 			if (this.prevchoice != 2) {
 				multiplier++;
 			}
-			temp = multiplier * multiplier * curious;
+			temp = Math.abs(multiplier * multiplier * curious);
+			total += temp;
+			values[ctr] = temp;
+			ctr++;
+			if (this.diam < growthLim)// !< is not a command so i changed it to >=
+			{
+				multiplier = 0;
+			}else{
+				multiplier = maternalInc;
+			}
+			//temp = (int) ((Math.atan(this.energy - maternalM)*100) + 1.5)* multiplier;// I had to make this an int
+			temp = (this.energy - maternalM) * multiplier;
+			if(temp < 0) {
+				temp = 0;
+			}
 			total += temp;
 			values[ctr] = temp;
 			ctr++;
 
-			temp = (int) (Math.atan(this.energy - maternalM) + 1.5) * maternalInc;// I had to make this an int
-			total += temp;
-			values[ctr] = temp;
-			ctr++;
-
-			int combined = -(this.energy * this.energy) + total + 5;
-			total += combined;
+			long combined = total;
 			int mindist = 999999999;// I DECREASED THIS NUMBER BECAUSE IT WAS TO LARGE TO BE AN INT
-			if (closestFood[0] != null) {
+			if (this.closestFood[2] != null) {
+				total += combined;
 				for (int i = 0; i < 3; i++) {
-					Food tempFood = closestFood[i];
+					Cell tempFood = closestFood[i];
 					int tempx = (int) Math.abs(tempFood.x - this.x);// these were also made ints
 					int tempy = (int) Math.abs(tempFood.y - this.y);// ''
 					int tempdist = (int) Math.sqrt((tempx * tempx) + (tempy * tempy));// ''
@@ -203,31 +224,31 @@ public class Organism extends Cell {
 				if (temp > combined) {
 					temp = combined;
 				}
+				values[ctr] = temp;
+				ctr++;
+				temp = combined - temp;
+				values[ctr] = temp;
+				ctr = 0;
 			} else {
 				temp = act;
 				total += values[1] + 5;
-				values[1] = values[1] * 2 + 5;
-				if (temp > combined) {
-					temp = combined;
-				}
-
-			}
-			values[ctr] = temp;
-			ctr++;
-			temp = combined - temp;
-			values[ctr] = temp;
-			ctr = 0;
-
-			Random rand = new Random();
-			int choiceNum = rand.nextInt(total) + 1;
-			while (ctr < 5 && choiceNum >= 0) {
-				choiceNum -= values[ctr];
+				values[1] = (values[1] * 2) + 5;
+				values[ctr] = 0;
 				ctr++;
+				values[ctr] = 0;
 			}
-			if (ctr <= 5 && choiceNum <= 0) {
+			Random rand = new Random();
+			if(total == 0) {
+				choice = 5;
+			}else {
+				long choiceNum = (rand.nextLong()%total) + 1;
+				ctr = 0;
+				while (ctr < 5 && choiceNum > 0) {
+					choiceNum -= values[ctr];
+					System.out.println(ctr + ": " + values[ctr]);
+					ctr++;
+				}
 				this.choice = ctr;
-			} else {
-				this.choice = -1;
 			}
 		} while (first);
 		this.prevchoice = result;
@@ -245,13 +266,140 @@ public class Organism extends Cell {
 		double distanceToFood2 = -1;
 
 		if (this.closestFood[0] == null) {
-			Food tempFood = closestFood[0];
+			Cell tempFood = closestFood[0];
 			int tempx = (int) Math.abs(tempFood.x - this.x);// these were also made ints
 			int tempy = (int) Math.abs(tempFood.y - this.y);// ''
 			int tempdist = (int) Math.sqrt((tempx * tempx) + (tempy * tempy));// ''
 
 		}
 		return 0;
+	}
+		
+	public void grow() {
+		double r = this.diam / 2;
+		double area = 3.14159*(r*r);
+		area += 25;			//CHECK
+		double newdiam = area/3.14159;
+		newdiam = Math.sqrt(newdiam) * 2;
+		newdiam += 1;
+		this.diam = (int) newdiam;
+		reduceEnergy(10);		//CHECK
+	}
+	/********************************************
+	 * updates the closest food coordinates
+	 *
+	 *
+	 *********************************************/
+	public void examine(LinkedList Organisms, LinkedList foodList, int numOrganisms, int numFood) {
+	    int mindist = 999999;
+	    int index1 = -1;
+	    int index2 = -1;
+	    for(int i = 0; i < 3; i++) {
+	    	Cell closeFood = null;
+		    for (int j = 0; j < numFood; j++) {
+		    	if(j != index1 && j != index2) {
+					Cell tempFood = (Cell) foodList.get(j);
+					int tempx = (int) Math.abs(tempFood.x - this.x);// these were also made ints
+					int tempy = (int) Math.abs(tempFood.y - this.y);// ''
+					int tempdist = (int) Math.sqrt((tempx * tempx) + (tempy * tempy));// ''
+					if (tempdist < mindist) {
+						mindist = tempdist;
+						closeFood = tempFood;
+						
+					}
+		    	}
+		    }
+		    
+	    	this.closestFood[i] = closeFood;
+	    	mindist = 999999;
+	    }
+	    for(int i = 0; i < 3; i++) {
+		    Cell closeOrganism = null;
+			for (int j = 0; j < numOrganisms; j++) {
+			    if(j != index1 || j != index2) {
+					Cell tempOrganism = (Cell) Organisms.get(j);
+					int tempx = (int) Math.abs(tempOrganism.x - this.x);// these were also made ints
+					int tempy = (int) Math.abs(tempOrganism.y - this.y);// ''
+					int tempdist = (int) Math.sqrt((tempx * tempx) + (tempy * tempy));// ''
+					if (tempdist < mindist) {
+						mindist = tempdist;
+						closeOrganism = tempOrganism;
+						
+					}
+			    }
+			}
+		    this.closestOrganism[i] = closeOrganism;
+		    mindist = 999999;
+	    }
+		reduceEnergy(2);		//CHECK
+	}
+	
+	public Organism split(){
+		int[] newTendencies = getTendencies();
+		Random rand = new Random();
+		int randNum;
+		int pos;
+		int multiplier;
+		for(int i = 0; i < 15; i++){
+			randNum = rand.nextInt(15);
+			pos = rand.nextInt(2);
+			multiplier = 1;
+			if(pos == 0){
+				multiplier = -1;
+			}
+			if(randNum >= 14){
+				newTendencies[i] += 3*multiplier;
+			}else if(randNum>= 12){
+				newTendencies[i] += 2*multiplier;
+			}else if(randNum >= 7){
+				newTendencies[i] += multiplier;
+			}
+		}
+		this.diam = this.diam/2;
+		this.energy = this.energy/2;
+		Color newColor = new Color(Math.abs((newTendencies[2]*3)%255), Math.abs((newTendencies[5]*3)%255), Math.abs((newTendencies[6]*3)%255));
+		Organism child = new Organism(this.x-1, this.y-1, this.diam, this.energy, newColor, newTendencies);
+		reduceEnergy(4);		//CHECK
+		return child;
+	}
+	
+	/********************************************
+	 * moves towards the closest food
+	 *
+	 *
+	 *********************************************/
+	public void move() {
+		if(closestFood[2] != null) {
+			if (energy > 0) {
+		
+			// Checks X
+			if (closestFood[0].x > (x + 5)) {
+				x += 5;
+				energy -= 1;
+			} else if (closestFood[0].x < (x - 5)) {
+				x -= 5;
+				energy -= 1;
+			}
+		}
+			if (energy > 0) {
+				// Checks y
+				if (closestFood[0].y > (y + 5)) {
+					y += 5;
+					energy -= 1;
+				} else if (closestFood[0].y < (y - 5)) {
+					y -= 5;
+					energy -= 1;
+				}
+			}
+		}
+	}
+	
+	public void idle(){
+		reduceEnergy(1);
+	}
+	
+	public void reduceEnergy(int x){
+		this.energy -= x;
 	}
 	/*
 	 * THIS SHOULD BE DONE IN THE PANNEL public void die(){ Food food = new
@@ -272,53 +420,12 @@ public class Organism extends Cell {
 	}
 
 	/********************************************
-	 * updates the closest food coordinates
-	 *
-	 *
-	 *********************************************/
-	public void examine(Organism[] x, Food[] y) {
-		for (int i = 0; i < 3; i++) {
-			this.closestOrganism[i] = x[i];
-			this.closestFood[i] = y[i];
-		}
-	}
-
-	/********************************************
-	 * moves towards the closest food
-	 *
-	 *
-	 *********************************************/
-	public void move() {
-
-		if (energy > 0) {
-			// Checks X
-			if (closestFood[0].x > (x + 5)) {
-				x += 5;
-				energy -= 1;
-			} else if (closestFood[0].x < (x - 5)) {
-				x -= 5;
-				energy -= 1;
-			}
-		}
-		if (energy > 0) {
-			// Checks y
-			if (closestFood[0].y > (y + 5)) {
-				y += 5;
-				energy -= 1;
-			} else if (closestFood[0].y < (y - 5)) {
-				y -= 5;
-				energy -= 1;
-			}
-		}
-	}
-
-	/********************************************
 	 * checks to see if the organism collects the food
 	 *
 	 *
 	 *********************************************/
 	public boolean collision(double foodX, double foodY, int foodEnergy) {
-		if (Math.abs(foodX - x) <= diam && Math.abs(foodY - y) <= diam) {
+		if (Math.abs(foodX - x) <= (diam/2) && Math.abs(foodY - y) <= (diam/2)) {
 			energy += foodEnergy;
 			return true;
 		}

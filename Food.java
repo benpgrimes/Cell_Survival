8493ec08c74a0a9e572cfb092128b3df978d2c72
tacomanjoe <47902704+tacomanjoe@users.noreply.github.com>
2019-04-15 13:03:24 -0400

    Update Food.java

--- Food.java
+++ Food.java
@@ -16,34 +16,34 @@ public class Food extends Cell{
   //****************************************************************
   public void draw(Graphics myBuffer) 
   {
-	int intx = (int) x;
-	int inty = (int) y;
-	int pointsx[] = new int [3];
-	int pointsy[] = new int [3];
-	//border for right triangle
-	myBuffer.setColor(color.BLACK);
-	pointsx[0] = intx + (int)diam/2 - 5 ; pointsx[1] = pointsx[0] + 27; pointsx[2] = pointsx[0] + 27;
-	pointsy[0] = inty + 8; pointsy[1] = pointsy[0] + 27; pointsy[2] = pointsy[0] - 27;
-    myBuffer.fillPolygon(new Polygon(pointsx, pointsy, 3));	
+    int intx = (int) x;
+    int inty = (int) y;
+    int pointsx[] = new int [3];
+    int pointsy[] = new int [3];
+    //border for right triangle
+    myBuffer.setColor(color.BLACK);
+    pointsx[0] = intx + (int)diam/2 - 5 ; pointsx[1] = pointsx[0] + 27; pointsx[2] = pointsx[0] + 27;
+    pointsy[0] = inty + 8; pointsy[1] = pointsy[0] + 27; pointsy[2] = pointsy[0] - 27;
+    myBuffer.fillPolygon(new Polygon(pointsx, pointsy, 3)); 
     //right triangle
-	myBuffer.setColor(color);
-	pointsx[0] = intx + (int)diam/2 +3; pointsx[1] = pointsx[0] + 17; pointsx[2] = pointsx[0] + 17;
-	pointsy[0] = inty + 8; pointsy[1] = pointsy[0] + 17; pointsy[2] = pointsy[0] - 17;
+    myBuffer.setColor(color);
+    pointsx[0] = intx + (int)diam/2 +3; pointsx[1] = pointsx[0] + 17; pointsx[2] = pointsx[0] + 17;
+    pointsy[0] = inty + 8; pointsy[1] = pointsy[0] + 17; pointsy[2] = pointsy[0] - 17;
     myBuffer.fillPolygon(new Polygon(pointsx, pointsy, 3));
     //border for left triangle
-	myBuffer.setColor(color.BLACK);
-	pointsx[0] = intx + (int)diam/2 + 5 ; pointsx[1] = pointsx[0] - 27; pointsx[2] = pointsx[0] - 27;
-	pointsy[0] = inty + 8; pointsy[1] = pointsy[0] + 27; pointsy[2] = pointsy[0] - 27;
-    myBuffer.fillPolygon(new Polygon(pointsx, pointsy, 3));	
+    myBuffer.setColor(color.BLACK);
+    pointsx[0] = intx + (int)diam/2 + 5 ; pointsx[1] = pointsx[0] - 27; pointsx[2] = pointsx[0] - 27;
+    pointsy[0] = inty + 8; pointsy[1] = pointsy[0] + 27; pointsy[2] = pointsy[0] - 27;
+    myBuffer.fillPolygon(new Polygon(pointsx, pointsy, 3)); 
     //left triangle
-	myBuffer.setColor(color);
-	pointsx[0] = intx + (int)diam/2 -3; pointsx[1] = pointsx[0] - 17; pointsx[2] = pointsx[0] - 17;
-	pointsy[0] = inty + 8; pointsy[1] = pointsy[0] + 17; pointsy[2] = pointsy[0] - 17;
+    myBuffer.setColor(color);
+    pointsx[0] = intx + (int)diam/2 -3; pointsx[1] = pointsx[0] - 17; pointsx[2] = pointsx[0] - 17;
+    pointsy[0] = inty + 8; pointsy[1] = pointsy[0] + 17; pointsy[2] = pointsy[0] - 17;
     myBuffer.fillPolygon(new Polygon(pointsx, pointsy, 3));
     //circle
-	myBuffer.setColor(color.BLACK);
+    myBuffer.setColor(color.BLACK);
     myBuffer.fillOval((int)x,(int)y,(int)diam, (int)diam);
-	myBuffer.setColor(color.WHITE);
+    myBuffer.setColor(color.WHITE);
     myBuffer.fillOval((int)x+4,(int)y+4,(int)diam-8, (int)diam-8);
   } 
   public double getX()
